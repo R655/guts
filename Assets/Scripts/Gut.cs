d7d19da1f723570118138106aed207f7214a4d5f
r655 <Rusty65536@gmail.com>
2014-05-02 18:40:28 +0600

    Изменение параметров хорьков от времени

--- Assets/Scripts/Gut.cs
+++ Assets/Scripts/Gut.cs
@@ -1,14 +1,61 @@
-using System;
+using System;	
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace AssemblyCSharp
 {
 	public class Gut
 	{
-		public Transform gameObject;
-		public Gut (Transform gutPrefab)
+		public enum State
 		{
-			gameObject = gutPrefab;
+			Sleep,
+			Stay,
+			Walk,
+			Eating,
+			Mating,
+			Hunting,
+			Stalking,
+			Dead
+		};
+		public enum Param
+		{
+			Age,
+			Starve,
+			Fatique,
+			Health
+		};
+
+
+
+		public Dictionary<Param, float> param = 
+			new Dictionary<Param, float>()
+			{
+				{Param.Health, 0.5f},
+				{Param.Age, 0.5f},
+				{Param.Starve, 0.3f},
+				{Param.Fatique, 0.1f}	
+			};
+
+		public Transform gutPrefab;
+		public State state = State.Stay;
+		public Transform gameObject;
+
+		public Gut (int i, int j)
+		{
+			gameObject = 
+				(Transform)GameObject.Instantiate(
+					gutPrefab, 
+					new Vector3(
+						0f,
+						0f,
+						0f), 
+					Quaternion.identity);
+			;
+		}
+
+		public void Death()
+		{
+			state = State.Dead;
 		}
 	}
 }
--- Assets/Scripts/GutLogics.cs
+++ Assets/Scripts/GutLogics.cs
@@ -8,19 +8,83 @@ namespace AssemblyCSharp
 	{
 		public Transform gutPrefab;
 
+		public Dictionary<Gut.Param, Dictionary<Gut.State, float>> speeds = 
+		new Dictionary<Gut.Param, Dictionary<Gut.State, float>>()
+		{
+			{
+				Gut.Param.Starve,
+				new Dictionary<Gut.State, float>() 
+				{
+					{Gut.State.Sleep, -0.1f},
+					{Gut.State.Walk, -0.5f},
+					{Gut.State.Stay, -0.3f},
+					{Gut.State.Eating, 0.3f}
+				}
+			},
+			{
+				Gut.Param.Age,
+				new Dictionary<Gut.State, float>() 
+				{
+					{Gut.State.Sleep, 0.3f},
+					{Gut.State.Walk, 0.3f},
+					{Gut.State.Stay, 0.3f},
+					{Gut.State.Eating, 0.3f}
+				}
+			},
+			{
+				Gut.Param.Fatique,
+				new Dictionary<Gut.State, float>() 
+				{
+					{Gut.State.Sleep, 0.5f},
+					{Gut.State.Walk, -0.5f},
+					{Gut.State.Stay, 0.1f},
+					{Gut.State.Eating, 0.1f}
+				}
+			}
+		};
+		public Dictionary<Gut.Param, float> maximums = 
+		new Dictionary<Gut.Param, float>()
+		{
+			{Gut.Param.Age, 50f},
+			{Gut.Param.Fatique, 50f},
+			{Gut.Param.Starve, 50f},
+			{Gut.Param.Health, 50f},
+		};
+
+		public Dictionary<Gut.Param, float> minimums = 
+		new Dictionary<Gut.Param, float>()
+		{
+			{Gut.Param.Age, 0f},
+			{Gut.Param.Fatique, 0f},
+			{Gut.Param.Starve, 0f},
+			{Gut.Param.Health, 0f},
+		};
+
 		LinkedList<Gut> guts;
 
 		void Start () {
 			guts = new LinkedList<Gut>();
 		}
-		
+
 		void Update () {
 			foreach(Gut gut in guts)
 			{
+				foreach(var par in gut.param)
+				{
+					gut.param[par.Key] += speeds[par.Key][gut.state]*Time.deltaTime;
 
+					if(par.Value > maximums[par.Key])
+						gut.param[par.Key] = maximums[par.Key];
+
+					if(par.Value < minimums[par.Key])
+					{
+						gut.param[par.Key] = minimums[par.Key];
+						gut.Death();
+						guts.Remove(gut);
+					}
+				}
 			}
-			// golod
-			// ustalost
+			
 		}
 	}
 }
